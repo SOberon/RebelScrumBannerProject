a10fa83c5fb9336f4fd562a6765ba13af00580d0
Justin Pauga <jpauga@msudenver.edu>
2017-09-12 13:57:02 -0600

    First commit

---
+++ src/test/java/com/rebelscrum/CS3250Project/Cs3250ProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rebelscrum.CS3250Project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Cs3250ProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
