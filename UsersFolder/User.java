5a558cdea80ae9004a7d03b250f7af70bf4ffeee
klphillips <31387691+klphillips@users.noreply.github.com>
2017-09-12 11:20:19 -0600

    Commiting User file to correct location

---
+++ UsersFolder/User.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class User(){
+
+    private string firstName;
+    private string lastName;
+    private int idNumber;
+    private string userEmail;
+
+    private User(string first, string last, int id, string email){
+	this.first = firstName;
+	this.last = lastName;
+	this.id =idNumber;
+	this.email = userEmail;
+    }
+
+    public string getFirstName(){
+	return firstName;
+    }
+    public string getLastName(){
+	return lastName;
+    }
+    public int getidNumber(){
+	return idNumber;
+    }
+    public string getEmail(){
+	return userEmail;
+    }
+}
