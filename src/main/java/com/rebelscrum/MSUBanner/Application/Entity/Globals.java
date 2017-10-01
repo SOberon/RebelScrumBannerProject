3a459f35fa174f8e317e49a9ca9d59024080f625
Sean O'Brien <msobrien90@gmail.com>
2017-10-01 09:26:35 -0600

    Fixed some of the globals logic; was only using 'if' statements before.

--- src/main/java/com/rebelscrum/MSUBanner/Application/Entity/Globals.java
+++ src/main/java/com/rebelscrum/MSUBanner/Application/Entity/Globals.java
@@ -1,4 +1,32 @@
 package com.rebelscrum.MSUBanner.Application.Entity;
 
 public class Globals {
+    private String university = "";
+
+    private final int FRESHMAN = 30;
+    private final int SOPHOMORE = 60;
+    private final int JUNIOR = 90;
+    private final int SENIOR = 120; //Unused because of how the current logic is written
+
+    private String status = "";
+
+    public String getMyLevel(int n){
+       if (n < FRESHMAN){
+           status = "Freshman";
+       }
+
+       else if (n < SOPHOMORE){
+           status = "Sophomore";
+       }
+
+       else if (n < JUNIOR){
+           status = "Junior";
+       }
+
+       else {
+           status = "Senior";
+       }
+
+       return status;
+    }
 }
